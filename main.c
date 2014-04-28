62d2f53340bfd5be613562c7d1bbb3ee718d0595
Andreslee <and66920@hotmail.com>
2014-04-28 11:56:08 -0500

    Despues de clonado

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 
 	printf("Hello %s!\n", name);
 	return 0;
-	/* Modificado por por Brayan David Cajica,
+	/* Modificado  por    Brayan David Cajica,
 	                  Christian Camilo Torres
 	                  Francisco Andres Guzman*/
 }
